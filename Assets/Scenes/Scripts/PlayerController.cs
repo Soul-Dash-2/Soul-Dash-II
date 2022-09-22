fc2924d1be812296ed47d0c94a628627940dcd6d
Phineas Ziegler <phinziegler@gmail.com>
2022-09-22 08:44:43 -0400

    improved basic dash

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     private float movementFactor;       // a value between -1 and 1, which determines the direction the player moves
     private bool canDash;
     private bool isDashing;             // true while the player is dashing
+    private bool isJumping;             // true after the player has executed a jump
     private string dashType = "basic";  // TODO: this should be an enum eventually
 
     // Public Movement variables
@@ -25,10 +26,11 @@ public class PlayerController : MonoBehaviour
     public float maxVelocityX;      // the player's maximum velocity on the x axis
     public float minimumVelocity;
 
-    public float percentFrictionX;  // % friction: how quickly does the player come to a halt.
+    public float percentFrictionX;  // % friction: how quickly does the player come to a halt (in the x direction)
+    public float percentFrictionY;
 
-    public float basicDashDistance; // How far the basic dash will carry you
-    public float dashVelocity;
+    public float basicDashTime;     // How long the dash lasts
+    public float basicDashVelocity; // How quickly does the basic dash move
 
     // Setup Code
     void Start()
@@ -51,8 +53,18 @@ public class PlayerController : MonoBehaviour
     // DeltaTime is not used, because the physics update occurs on a fixed interval, and is therefore not bound to the deltaTime
     void FixedUpdate()
     {
-        float xVel = calculateXVelocity();
-        rb.velocity = new Vector2(xVel, rb.velocity.y);
+        if (rb.velocity.y <= 0)
+        {
+            isGrounded = true;
+        }
+
+        float xVel = CalculateXVelocity();
+        float yVel = CalculateYVelocity();
+        rb.velocity = new Vector2(xVel, yVel);
+
+
+        Debug.Log("X-Vel: " + xVel);
+        Debug.Log("Y-Vel: " + yVel);
     }
 
     // Enable and Disable events
@@ -77,88 +89,110 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = true;
             canDash = true;
+            isJumping = false;
         }
     }
 
     // Gets the friction factor -- multiplied with the xVelocity 
-    float getFrictionFactor()
+    float GetFrictionFactorX()
     {
         // if accelerating in the same direction as velocity, don't count friction
-        if (Mathf.Ceil(Mathf.Clamp(movementFactor, -1, 1)) == Mathf.Ceil(Mathf.Clamp(rb.velocity.x, -1, 1)) || isDashing)
+        if ((Mathf.Ceil(Mathf.Clamp(movementFactor, -1, 1)) == Mathf.Ceil(Mathf.Clamp(rb.velocity.x, -1, 1))) || isDashing)
         {  // FIXME: this will likely NOT work on controller
             return 1;
         }
         return 1 - (percentFrictionX / 100);
     }
 
-    // Calculate X Velocity
-    float calculateXVelocity()
+    float GetFrictionFactorY()
     {
-        float newVel = (rb.velocity.x + (movementFactor * accelerationX)) * getFrictionFactor();        // FIXME: this does not account for controller deadzone
+        // if dashing, jumping, or falling, do not apply friction
+        if (isDashing || isJumping || rb.velocity.y < 0)
+        {
+            return 1;
+        }
+        return 1 - (percentFrictionY / 100);
+    }
+
+
+    // Calculate X Velocity
+    float CalculateXVelocity()
+    {
+        float newVel = (rb.velocity.x + (movementFactor * accelerationX)) * GetFrictionFactorX();        // FIXME: this does not account for controller deadzone
         // If the player is dashing, no need to clamp velocity
-        if (isDashing) {
+        if (isDashing)
+        {
             return newVel;
         }
         // Set velocity to zero when velocity is small
-        if (Mathf.Abs(newVel) < minimumVelocity) {
+        if (Mathf.Abs(newVel) < minimumVelocity)
+        {
             return 0;
         }
 
         return Mathf.Clamp(newVel, -maxVelocityX, maxVelocityX);
     }
 
+    float CalculateYVelocity()
+    {
+        return rb.velocity.y * GetFrictionFactorY();
+    }
+
     // Jump event
     void Jump()
     {
         // Can only jump if on the ground
-        if (isGrounded)
+        if (isGrounded || !isDashing)
         {
+            isJumping = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
         }
     }
 
     // Whether or not the player is allowed to dash -- prefer this method over the boolean canDash
-    bool CanDash() {
+    bool CanDash()
+    {
         return canDash || isGrounded;
     }
 
     // Dash event --> executes the various dash type coroutines
     void Dash()
     {
-        if(!CanDash()) {
+        if (!CanDash())
+        {
             return;
         }
-        if(dashType == "basic") {
+        isJumping = false;
+        if (dashType == "basic")
+        {
             StartCoroutine(BasicDash());
         }
     }
 
     // Get the direction for a dash based on the mouse location
-    Vector2 GetDirection() {
+    Vector2 GetDirection()
+    {
         Vector2 pos = rb.position;
         Vector2 mouse = playerCamera.ScreenToWorldPoint(Input.mousePosition);
         return (mouse - pos).normalized;
     }
 
-    // Basic dash -- this needs a lot of work unfortunately
-    IEnumerator BasicDash() {
+    // Basic dash
+    IEnumerator BasicDash()
+    {
         Vector2 direction = GetDirection();
-        Vector2 pos = rb.position;
+        Vector2 ogVelocity = rb.velocity;
 
-        rb.gravityScale = 0;
         isDashing = true;
         canDash = false;
 
-        float travelled = 0;
-        float gravityTemp = gravityScale;
-        while(travelled < basicDashDistance) {
-            travelled = Vector2.Distance(pos, rb.position);
-            rb.velocity = direction * dashVelocity;
+        float dashTime = 0;
+        while (dashTime < basicDashTime)
+        {
+            dashTime += Time.deltaTime;
+            rb.velocity = direction * basicDashVelocity;
             yield return null;
         }
-
-        rb.velocity = new Vector2(0, 0);
-        rb.gravityScale = gravityScale;
         isDashing = false;
     }
 }
