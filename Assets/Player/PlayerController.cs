0126e68658b6ca312b9f1ea9c4c9a7f8b6fb1074
Phineas Ziegler <phinziegler@gmail.com>
2022-10-11 02:40:28 -0400

    fixed bugs which caused jump to be lost

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -34,6 +34,7 @@ public class PlayerController : MonoBehaviour
     private bool isTouching;            // true while player is touching anything
     private bool canGlide;              // true while the player has the ability to glide --> granted by eyeball dash
     private bool isTouchingWallGround;   // true while player is touching the wall or the ground
+    private float prevYVel;             // the value representing the players velocity on the last fixed update
 
     //Damage things
     public float dashDamage;
@@ -106,6 +107,7 @@ public class PlayerController : MonoBehaviour
     {
         float xVel = CalculateXVelocity();
         float yVel = CalculateYVelocity();
+        prevYVel = yVel;
         rb.velocity = new Vector2(xVel, yVel);
 
         HandleShortHop(yVel);
@@ -236,7 +238,7 @@ public class PlayerController : MonoBehaviour
     void Jump()
     {
         // Can only jump if on the ground
-        if (isGrounded)// && !isDashing)
+        if (onGround())// && !isDashing)
         {
             isJumping = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
@@ -257,7 +259,7 @@ public class PlayerController : MonoBehaviour
     //Crouch (or fastfall)
     void Crouch()
     {
-        if (isGrounded)
+        if (onGround())
         {
             //Add in crouch hitbox
             //Maybe slow the player?
@@ -288,7 +290,12 @@ public class PlayerController : MonoBehaviour
     // Whether or not the player is allowed to dash -- prefer this method over the boolean canDash
     public bool CanDash()
     {
-        return canDash || isGrounded;
+        return canDash || onGround();
+    }
+
+    // replaces uses of 'isGrounded' to check for feet on the floor.
+    public bool onGround() {
+        return (rb.velocity.y == 0) && (prevYVel == 0);
     }
 
     // Dash event --> executes the various dash type coroutines
