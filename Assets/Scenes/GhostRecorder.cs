b58f1f4b22d2c0a29408d42e5276cd53b3f9ab1c
yxd421 <yxd421@case.edu>
2022-10-29 23:17:53 -0400

    finish ghost recorder

--- Assets/Scenes/GhostRecorder.cs
+++ Assets/Scenes/GhostRecorder.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine.Profiling;
+using UnityEngine.InputSystem;
 
 public class GhostRecorder : MonoBehaviour
 {
+
     private GameObjectRecorder recorder;
 
     [SerializeField] string saveLocation = "Assets/GhostRecords/";
     [SerializeField] string clipName;
-    [SerializeField] string startRecordKey = "i";
-    [SerializeField] string stopRecordKey = "o";
-    [SerializeField] string _deleteRecKey = "p";
+    [SerializeField] InputAction startRecordButton;
+    [SerializeField] InputAction stopRecordButton;
     [SerializeField] GameObject recordedObject;
-    string currentClipName;
 
     private AnimationClip clip;
-    private AnimationClip currentClip;
-    private bool _canRecord = true;
-    int index = 0;
+    private bool canRecord = true;
 
     private void CreateNewClip()
     {
@@ -29,22 +27,69 @@ public class GhostRecorder : MonoBehaviour
         clip.frameRate = 60;
     }
 
-   private void OnEnable()
+    private void OnEnable()
     {
-        CreateNewClip();
+        if (clip == null)
+            CreateNewClip();
 
     }
-// Start is called before the first frame update
-void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         recorder = new GameObjectRecorder(recordedObject);
         recorder.BindComponentsOfType<Transform>(recordedObject, true);
-        clipName = recordedObject.name;
+
+        startRecordButton = new InputAction(
+            type: InputActionType.Button,
+            binding: "<Keyboard>/i",
+            interactions: "press(behavior=1)");
+        startRecordButton.Enable();
+        stopRecordButton = new InputAction(
+            type: InputActionType.Button,
+            binding: "<Keyboard>/o",
+            interactions: "press(behavior=1)");
+        stopRecordButton.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startRecordButton.triggered)
+            StartRecording();
+        if (stopRecordButton.triggered)
+            StopRecording();
     }
+
+    private void StartRecording()
+    {
+        //allow the GameObjectRecorder to start recording
+        canRecord = true;
+        CreateNewClip();
+        Debug.Log("Animation Recording for " + gameObject.name + " has STARTED");
+
+    }
+
+    private void StopRecording()
+    {
+        Debug.Log("Animation Recording for " + gameObject + " has STOPPED");
+        //stop recording
+        canRecord = false;
+        recorder.SaveToClip(clip); 
+        AssetDatabase.CreateAsset(clip, saveLocation + clipName + ".anim");
+        Debug.Log(saveLocation + clipName + ".anim");
+        AssetDatabase.SaveAssets();
+    }
+
+
+
+    private void LateUpdate()
+    {
+        if (clip == null) return;
+        if (canRecord)
+        { //the GameObjectRecorder will start recording
+            recorder.TakeSnapshot(Time.deltaTime);
+        }
+    }
+
+
 }
