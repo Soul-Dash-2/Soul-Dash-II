ea21246ef3d440097d2f80705facfc4d6e9920e0
yxd421 <yxd421@case.edu>
2022-11-08 20:30:07 -0500

    fix bugs in sfx manager v2

--- Assets/Resources/Audio/SFX_manager.cs
+++ Assets/Resources/Audio/SFX_manager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SFX_manager : MonoBehaviour
 {
-    static AudioSource audioSource;
+    private AudioSource audioSource;
     [SerializeField]  AudioClip basicDash;
 
     
