bbac1155064ae2dff280f93e3aebc0b4e2d115a7
luiskeniosis <luiskeniosis@gmail.com>
2022-11-14 23:26:49 -0500

    Credit scene now fades in and out, and returns to the main menu after scroll is complete.

---
+++ Assets/Scenes/Scripts/CreditsEnd.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsEnd : MonoBehaviour
+{
+    public string sceneToLoad;
+    private float timer = 12f;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            Application.LoadLevel(sceneToLoad);
+        }
+    }
+}
