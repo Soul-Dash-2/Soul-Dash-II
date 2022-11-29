3931c999b114aff2c0a8b720705d81b6e710d20a
Xochitl Villalvazo <dxv135@case.edu>
2022-11-29 12:10:18 -0500

    Added lvl3 -> credit screen functionality

--- Assets/finalSceneTrigger.cs
+++ Assets/finalSceneTrigger.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class finalSceneTrigger : MonoBehaviour
 {
-	[SerializeField] private GameObject hero_ref;
+	[SerializeField] private PlayerController hero_ref;
 	[SerializeField] private GameObject pos_obj;
 	private Vector3 final_pos;
+	private PlayerController player_ctrl;
+	[SerializeField] private pauseScreenToggle pst;
 	
     // Start is called before the first frame update
     void Start()
     {
+		player_ctrl = hero_ref.GetComponent<PlayerController>();
         final_pos = pos_obj.transform.position;
     }
 
@@ -25,6 +28,9 @@ public class finalSceneTrigger : MonoBehaviour
         Debug.Log(col.tag);
         if (col.CompareTag("Player"))
         {
+			//player_ctrl.Disable();
+			player_ctrl.onDisable();
+			pst.RemoveGUI();
             hero_ref.transform.position = final_pos;
             //GameObject.Find("LevelController").SendMessage("LoadNextScene", true);
         }
